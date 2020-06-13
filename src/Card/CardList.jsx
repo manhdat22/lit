a205370d66a745992e47fc998cb18c13bce12c52
le.manh.dat <le.manh.dat@sun-asterisk.com>
2020-06-13 09:59:47 +0700

    Handle state completed

--- src/Card/CardList.jsx
+++ src/Card/CardList.jsx
@@ -1,9 +1,9 @@
-/* global chrome */ 
+/* global chrome */
 
 import React from 'react';
 import Card from './Card';
 
-class Cards extends React.Component {
+class CardList extends React.Component {
   constructor(props) {
     super(props);
     this.state = { cardList: null };
@@ -11,30 +11,26 @@ class Cards extends React.Component {
 
   componentDidMount() {
     chrome.storage.sync.get(function (obj) {
-      let data = Object.keys(obj).map((key) => {
-        let id = {id: key}
-        let data = JSON.parse(obj[key])
-        
-        return (data.type === 'card') ? {...id, ...data} : null;
-      }).filter((card) => {return card != null});
-      
-      this.setState({cardList: data})
+      var cardList = [];
+      Object.keys(obj).forEach((key) => {
+        const id = {id: key}
+        const data = JSON.parse(obj[key])
+        if (data.type === 'card') cardList.push({...id, ...data});
+      });
+      this.setState({cardList: cardList});
     }.bind(this));
-  }
+  };
 
   render() {
-    console.log(this.state.cardList);
-    if (this.state.cardList) {
-      return (
-        <>
-          {this.state.cardList.map(card => (
-            <Card key={card.id} id={card.id} title={card.title} />
-          ))}
-        </>
-      );
-    }
-    return (<span>Loading...</span>)
+    if (!this.state.cardList) return (<span>Loading...</span>)
+    return (
+      <>
+        {this.state.cardList.map(card => (
+          <Card key={card.id} id={card.id} title={card.title} />
+        ))}
+      </>
+    );
   };
 };
 
-export default Cards;
+export default CardList;
